67ae1c00e3efe1b0923b0c27ef2fa4eba1b89618
keziatamus <flaviana.tamus@gmail.com>
2020-07-17 15:58:23 +0800

    Initial commit

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack'; //npm install @react-navigation/stack
+import LoginScreen from './screens/LoginScreen';
+import SignUpScreen from './screens/SignUpScreen';
+import ForgotPassword from './screens/ForgotPassword';
+import CodeConfirmation  from './screens/CodeConfirmation'; 
+import HomePage from './screens/feeds/HomePage'; 
+
+const Stack = createStackNavigator();
+
+function App() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Foodmate"> 
+          <Stack.Screen name="Log In" component={LoginScreen}/>
+          <Stack.Screen name="Sign Up" component={SignUpScreen}/>
+          <Stack.Screen name="Forgot Password" component={ForgotPassword}/>  
+          <Stack.Screen name="Code Confirmation" component={CodeConfirmation}/>  
+          <Stack.Screen name="Foodmate" component={HomePage}/>
+          </Stack.Navigator>
+      </NavigationContainer>
+    );
+}
+
+export default App;
