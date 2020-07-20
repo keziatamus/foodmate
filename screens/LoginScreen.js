48a0044e768d9f57dc87cb711c009402724dffa9
keziatamus <flaviana.tamus@gmail.com>
2020-07-20 14:43:14 +0800

    Authentication with Firebase

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,45 +1,71 @@
-import React from 'react';
+import * as React from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity, Button } from 'react-native';
+import global from '../global';
 
-export default class App extends React.Component {
-  state={
-    email:"",
-    password:""
+export default class LoginScreen extends React.Component {
+  state = {
+    email: "",
+    password: "",
+    user: null,
+  };
+
+  componentDidMount() {
+    global.firebase.auth().onAuthStateChanged(
+      (user) => this.setState({ user: user })
+    );
   }
-  render(){
+
+  render() {
+
     return (
       <View style={styles.container}>
         <Text style={styles.logo}>foodmate</Text>
         <View style={styles.inputView} >
-          <TextInput  
+          <TextInput
             style={styles.inputText}
-            placeholder="Username" 
+            placeholder="Username"
             placeholderTextColor="#707070"
-            onChangeText={text => this.setState({email:text})}/>
+            value={this.state.email}
+            onChangeText={text => this.setState({ email: text })} />
         </View>
         <View style={styles.inputView} >
-          <TextInput  
+          <TextInput
             secureTextEntry
             style={styles.inputText}
-            placeholder="Password" 
+            placeholder="Password"
             placeholderTextColor="#707070"
-            onChangeText={text => this.setState({password:text})}/>
+            value={this.state.password}
+            onChangeText={text => this.setState({ password: text })} />
         </View>
         <TouchableOpacity>
           <Text onPress={() => this.props.navigation.navigate('Forgot Password')}
-          style={styles.forgot}>Forgot Password?</Text>
+            style={styles.forgot}>Forgot Password?</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.loginBtn}>
+        <TouchableOpacity
+          style={styles.loginBtn}
+          onPress={() => this.login()}>
           <Text>Log In</Text>
         </TouchableOpacity>
         <TouchableOpacity>
-        <Text onPress={() => this.props.navigation.navigate('Sign Up')}
-        style={styles.signUpText}>Sign Up
+          <Text onPress={() => this.props.navigation.navigate('Sign Up')}
+            style={styles.signUpText}>Sign Up
         </Text>
         </TouchableOpacity>
 
       </View>
     );
+  }
+
+  async login() {
+    try {
+      await global.firebase.auth().signInWithEmailAndPassword(
+        this.state.email,
+        this.state.password,
+      );
+      alert("Logged in!");
+    } catch (error) {
+      alert(error);
+    }
   }
 }
 
@@ -50,43 +76,43 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  logo:{
-    fontWeight:"bold",
-    fontSize:50,
-    color:'black',
-    marginBottom:40,
+  logo: {
+    fontWeight: "bold",
+    fontSize: 50,
+    color: 'black',
+    marginBottom: 40,
     letterSpacing: 3
   },
-  inputView:{
-    width:"80%",
-    backgroundColor:"#FAF7F0",
-    borderRadius:20,
-    height:50,
-    marginBottom:20,
-    justifyContent:"center",
-    padding:20
+  inputView: {
+    width: "80%",
+    backgroundColor: "#FAF7F0",
+    borderRadius: 20,
+    height: 50,
+    marginBottom: 20,
+    justifyContent: "center",
+    padding: 20
   },
-  inputText:{
-    height:50,
-    color:"black"
+  inputText: {
+    height: 50,
+    color: "black"
   },
-  forgot:{
-    color:"#FBAF02",
-    fontSize:10,
+  forgot: {
+    color: "#FBAF02",
+    fontSize: 10,
     marginLeft: 200
   },
-  loginBtn:{
-    width:"80%",
-    backgroundColor:"#FBAF02",
-    borderRadius:20,
-    height:50,
-    alignItems:"center",
-    justifyContent:"center",
-    marginTop:50,
-    marginBottom:10
+  loginBtn: {
+    width: "80%",
+    backgroundColor: "#FBAF02",
+    borderRadius: 20,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 50,
+    marginBottom: 10
   },
-  signUpText:{
-    fontSize:13,
-    color:"#FBAF02"
+  signUpText: {
+    fontSize: 13,
+    color: "#FBAF02"
   }
 });
