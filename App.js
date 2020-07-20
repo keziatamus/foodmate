2b7ae10176c4d4de673559d6cf44608a917bb84e
keziatamus <flaviana.tamus@gmail.com>
2020-07-20 17:04:40 +0800

    Working on Profile Screen

--- App.js
+++ App.js
@@ -6,18 +6,20 @@ import SignUpScreen from './screens/SignUpScreen';
 import ForgotPassword from './screens/ForgotPassword';
 import CodeConfirmation  from './screens/CodeConfirmation'; 
 import HomePage from './screens/feeds/HomePage'; 
+import UserProfile from './screens/feeds/UserProfile'; 
 
 const Stack = createStackNavigator();
 
 function App() {
     return (
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Foodmate"> 
+        <Stack.Navigator initialRouteName="Profile"> 
           <Stack.Screen name="Log In" component={LoginScreen}/>
           <Stack.Screen name="Sign Up" component={SignUpScreen}/>
           <Stack.Screen name="Forgot Password" component={ForgotPassword}/>  
           <Stack.Screen name="Code Confirmation" component={CodeConfirmation}/>  
           <Stack.Screen name="Foodmate" component={HomePage}/>
+          <Stack.Screen name="Profile" component={UserProfile}/>
           </Stack.Navigator>
       </NavigationContainer>
     );
