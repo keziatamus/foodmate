19f341ffcf5ce04b7d3ec8c10ce4824ca6ec925e
keziatamus <flaviana.tamus@gmail.com>
2020-07-22 13:21:53 +0800

    Finish Create Event page

--- App.js
+++ App.js
@@ -7,19 +7,21 @@ import ForgotPassword from './screens/ForgotPassword';
 import CodeConfirmation  from './screens/CodeConfirmation'; 
 import HomePage from './screens/feeds/HomePage'; 
 import CreateEvent from './screens/feeds/CreateEvent'; 
+import GoogleMap from './screens/feeds/GoogleMap'; 
 
 const Stack = createStackNavigator();
 
 function App() {
     return (
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Create"> 
+        <Stack.Navigator initialRouteName="Map"> 
           <Stack.Screen name="Log In" component={LoginScreen}/>
           <Stack.Screen name="Sign Up" component={SignUpScreen}/>
           <Stack.Screen name="Forgot Password" component={ForgotPassword}/>  
           <Stack.Screen name="Code Confirmation" component={CodeConfirmation}/>  
           <Stack.Screen name="Foodmate" component={HomePage}/>
           <Stack.Screen name="Create" component={CreateEvent}/>
+          <Stack.Screen name="Map" component={GoogleMap}/>
           </Stack.Navigator>
       </NavigationContainer>
     );
--- screens/feeds/CreateEvent.js
+++ screens/feeds/CreateEvent.js
@@ -1,46 +1,198 @@
+
 import React, {useState} from 'react';
-import {View, Button, Platform} from 'react-native';
-import DateTimePicker from '@react-native-community/datetimepicker';
+import DateTimePickerModal from "react-native-modal-datetime-picker";
+import RNPickerSelect, { defaultStyles } from 'react-native-picker-select';  //npm install react-native-picker-select
+import DatePicker from 'react-native-datepicker';
+import { StyleSheet, Text, View, Button, TextInput, TouchableOpacity, SafeAreaView } from 'react-native';
+import moment from 'moment';
 
-const App = () => {
-  const [date, setDate] = useState(new Date(1598051730000));
-  const [mode, setMode] = useState('date');
-  const [show, setShow] = useState(false);
+export default class App extends React.Component {
+    constructor(){
+        super()
+        this.state = {
+            isVisible: false
+        }
+    }
 
-  const onChange = (event, selectedDate) => {
-    const currentDate = selectedDate || date;
-    setShow(Platform.OS === 'ios');
-    setDate(currentDate);
-  };
+    handlePicker = () => {
+        this.setState({
+            isVisible: false
+        })
+    }
 
-  const showMode = currentMode => {
-    setShow(true);
-    setMode(currentMode);
-  };
+    hidePicker = () => {
+        this.setState({
+            isVisible: false
+        })
+    }
 
+    state = {
+    title: "",
+    desc: "",
+  }
 
-  const showTimepicker = () => {
-    showMode('time');
-  };
+  render() {
 
-  return (
-    <View>
-      <View>
-        <Button onPress={showTimepicker} title="Show time picker!" />
-      </View>
-      {show && (
-        <DateTimePicker
-          testID="dateTimePicker"
-          timeZoneOffsetInMinutes={0}
-          value={date}
-          mode={mode}
-          is24Hour={true}
-          display="default"
-          onChange={onChange}
+    return (
+
+      <SafeAreaView style={styles.container}>
+        <View style={styles.signUpView}>
+          <Text style={styles.signUpText}> Create Event</Text>
+        </View>
+        <View style={styles.inputView} >
+          <TextInput
+            style={styles.inputText}
+            placeholder="Title"
+            placeholderTextColor="#707070"
+            onChangeText={text => this.setState({ title: text })} />
+        </View>
+        <View style={styles.inputView} >
+          <TextInput
+            style={styles.inputText}
+            placeholder="Description"
+            placeholderTextColor="#707070"
+            onChangeText={text => this.setState({ desc: text })} />
+        </View>
+
+        <DatePicker
+          showIcon={false}
+          androidMode="spinner"
+          style={styles.inputView}
+          date={this.state.date}
+          mode="date"
+          placeholder="Select"
+          format="DD-MM-YYYY"
+          minDate={moment().format('DD-MM-YYYY')}
+          confirmBtnText="Confirm"
+          cancelBtnText="Cancel"
+          customStyles={{
+            dateInput: {
+              backgroundColor: "#FAF7F0",
+              borderRadius: 20,
+              borderLeftWidth: 0,
+              borderRightWidth: 0,
+              borderTopWidth: 0,
+              borderBottomWidth: 0,
+              alignItems: 'flex-start',
+            },
+            placeholderText: {
+              color: "#707070"
+            }
+
+          }}
+          onDateChange={(date) => {
+            this.setState({ date: date });
+          }}
         />
-      )}
-    </View>
-  );
-};
 
-export default App;
+        <DatePicker
+          showIcon={false}
+          androidMode="spinner"
+          style={styles.inputView}
+          date={this.state.date}
+          mode="time"
+          placeholder="Select time"
+          format="DD-MM-YYYY"
+          minDate={moment().format('DD-MM-YYYY')}
+          confirmBtnText="Confirm"
+          cancelBtnText="Cancel"
+          customStyles={{
+            dateInput: {
+              backgroundColor: "#FAF7F0",
+              borderRadius: 20,
+              borderLeftWidth: 0,
+              borderRightWidth: 0,
+              borderTopWidth: 0,
+              borderBottomWidth: 0,
+              alignItems: 'flex-start',
+            },
+            placeholderText: {
+              color: "#707070"
+            }
+
+          }}
+          onDateChange={(date) => {
+            this.setState({ date: date });
+          }}
+        />
+        <TouchableOpacity
+          style={styles.loginBtn}>
+          <Text> Confirm </Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+
+  },
+  inputView: {
+    width: "80%",
+    backgroundColor: "#FAF7F0",
+    borderRadius: 20,
+    height: 20,
+    marginBottom: 20,
+    justifyContent: "center",
+    padding: 20
+  },
+  inputText: {
+    height: 50,
+    color: "black"
+  },
+  loginBtn: {
+    width: "80%",
+    backgroundColor: "#FBAF02",
+    borderRadius: 20,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    marginBottom: 10
+  },
+  buttonText: {
+    fontSize: 16,
+    color: "white"
+  },
+  signUpText: {
+    fontSize: 18,
+    color: "black",
+    fontWeight: 'bold',
+    marginBottom: 30
+  },
+  signUpView: {
+    paddingBottom: 30,
+  },
+
+});
+
+const pickerSelectStyles = StyleSheet.create({
+  placeholder: {
+    color: '#707070',
+  },
+  inputIOS: {
+    alignSelf: 'center',
+    width: '80%',
+    backgroundColor: "#FAF7F0",
+    borderRadius: 20,
+    padding: 20,
+    fontSize: 15,
+    paddingVertical: 12,
+    paddingRight: 30, // to ensure the text is never behind the icon
+  },
+  inputAndroid: {
+    alignSelf: 'center',
+    width: '80%',
+    backgroundColor: "#FAF7F0",
+    borderRadius: 20,
+    padding: 20,
+    fontSize: 15,
+    paddingVertical: 12,
+    paddingRight: 30, // to ensure the text is never behind the icon
+  },
+});
