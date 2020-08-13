3afec6c855d3593d9827375fca9ac5d663d4c381
keziatamus <65693754+keziatamus@users.noreply.github.com>
2020-08-13 22:05:39 +0800

    Update TopCategories.js

--- screens/feeds/TopCategories.js
+++ screens/feeds/TopCategories.js
@@ -4,15 +4,18 @@ import { View, Text, StyleSheet, Image } from 'react-native';
 class TopCategories extends Component{
     render(){
         return(
-            <View style={{width:250, height:230, marginLeft:20}}>
+            <View style={{width:250, height:230, paddingLeft:20}}>
                     <View style={{flex:2}}>
                       <Image source={this.props.imageUri}  
                       style={{flex:1, width:null, height:null, resizeMode:'cover', borderRadius:10, marginVertical:10}}/>
 
                     </View>
-                    <View style={{flex:1, paddingLeft:0}}>
-                      <Text style={{fontSize:14, fontWeight:'600', marginBottom:3}}>{this.props.name}</Text>
-                      <Text style={{fontsize:14, fontWeight:'300', marginBottom:3}}>{this.props.menu}</Text>
+                    <View style={{flexDirection:'row',justifyContent:'space-between'}}>
+                      <Text style={{fontSize:15, fontWeight:'600'}}>{this.props.name}</Text>
+                      <Text style={{fontSize:13, fontWeight:'400'}}>{this.props.total}</Text>
+                    </View>
+                    <View style={{flex:1,paddingTop:5}}>
+                    <Text style={{fontsize:14, fontWeight:'300'}}>{this.props.menu}</Text>
                     </View>
                   </View>
         )
