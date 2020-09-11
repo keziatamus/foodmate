ccd07007824b2b638e93b84497f1e7a7ee35e893
keziatamus <65693754+keziatamus@users.noreply.github.com>
2020-09-11 21:06:34 +0800

    Reset password through email

--- screens/ForgotPassword.js
+++ screens/ForgotPassword.js
@@ -1,86 +1,102 @@
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
-import { Feather } from '@expo/vector-icons';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import { Feather } from "@expo/vector-icons";
+import global from "../global";
 
-export default class App extends React.Component {
-  state={
-    email:"",
-  }
-  render(){
-    return (
-        <View style={styles.container}>
-        <TouchableOpacity
-          onPress={() => this.props.navigation.goBack()}
-          style={{position:'absolute', left:20, top:40}}>
-          <Feather name='arrow-left' size={24} color='black'/>
-        </TouchableOpacity>
-         <TouchableOpacity>
-             <Text style={styles.titleText}>Trouble logging in?</Text>
-        </TouchableOpacity>
-        <TouchableOpacity>
-             <Text style={styles.baseText}>Enter your email and we'll send you a link to get back into your account.</Text>
-        </TouchableOpacity>   
-        <View style={styles.inputView} >
-          <TextInput  
-            style={styles.inputText}
-            placeholder="Email" 
-            placeholderTextColor="#707070"
-            onChangeText={text => this.setState({email:text})}/>
-        </View>
-        <TouchableOpacity style={styles.button} onPress={() => this.props.navigation.navigate ("Code Confirmation" )}>
-        <Text>Next</Text>
-        </TouchableOpacity>
+export default function Reset({ navigation }) {
+  const [email, setEmail] = useState("");
 
+  const reset = async () => {
+    try {
+      await global.firebase.auth().sendPasswordResetEmail(email);
+    } catch (e) {
+      Alert.alert(e.message);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity
+        onPress={() => this.props.navigation.goBack()}
+        style={{ position: "absolute", left: 20, top: 40 }}
+      >
+        <Feather name="arrow-left" size={24} color="black" />
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text style={styles.titleText}>Trouble logging in?</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text style={styles.baseText}>
+          Enter your email and we'll send you a link to get back into your
+          account.
+        </Text>
+      </TouchableOpacity>
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.inputText}
+          placeholder="Email"
+          placeholderTextColor="#707070"
+          value={email}
+          onChangeText={setEmail}
+        />
+      </View>
+      <TouchableOpacity style={styles.button} onPress={() => reset()}>
+        <Text>Send Email</Text>
+      </TouchableOpacity>
     </View>
-    );
-  }
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  inputView:{
-    width:"80%",
-    backgroundColor:"#FAF7F0",
-    borderRadius:20,
-    height:50,
-    marginTop:60,
-    marginBottom:0,
-    justifyContent:"center",
-    padding:20
+  inputView: {
+    width: "80%",
+    backgroundColor: "#FAF7F0",
+    borderRadius: 20,
+    height: 50,
+    marginTop: 60,
+    marginBottom: 0,
+    justifyContent: "center",
+    padding: 20,
   },
-  inputText:{
-    height:50,
-    color:"black"
+  inputText: {
+    height: 50,
+    color: "black",
   },
-  button:{
-    width:"80%",
-    backgroundColor:"#FBAF02",
-    borderRadius:20,
-    height:50,
-    alignItems:"center",
-    justifyContent:"center",
-    marginTop:20,
-    marginBottom:10
+  button: {
+    width: "80%",
+    backgroundColor: "#FBAF02",
+    borderRadius: 20,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 20,
+    marginBottom: 10,
   },
-  titleText:{
-    fontSize:20,
-    color:"black",
-    fontWeight: 'bold',
-    textAlign:'center',
-    marginBottom:10,
-    paddingHorizontal: "10%"
+  titleText: {
+    fontSize: 20,
+    color: "black",
+    fontWeight: "bold",
+    textAlign: "center",
+    marginBottom: 10,
+    paddingHorizontal: "10%",
   },
-  baseText:{
-    fontSize:14,
-    color:"black",
-    textAlign:'center',
-    marginBottom:10,
-    paddingHorizontal: "10%"
-  }
+  baseText: {
+    fontSize: 14,
+    color: "black",
+    textAlign: "center",
+    marginBottom: 10,
+    paddingHorizontal: "10%",
+  },
 });
-
