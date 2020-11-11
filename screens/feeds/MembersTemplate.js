10f8716115af1cf6fba73c57de8a08c74ba8349f
keziatamus <65693754+keziatamus@users.noreply.github.com>
2020-11-11 22:45:19 +0800

    Update MembersTemplate.js

--- screens/feeds/MembersTemplate.js
+++ screens/feeds/MembersTemplate.js
@@ -1,28 +1,28 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
 import { View, Text, StyleSheet, Image, ImageBackground } from 'react-native';
 
-export default class Members extends Component{
+export default class Members extends Component {
     state = {
         image: null,
-      };
+    };
 
-    render(){
+    render() {
         let { image } = this.state;
-        return(
-            <View style={{flex:1, backgroundColor: 'white'}}>
-                <View style={{flexDirection:'row'}}>
-                <ImageBackground 
-                    style={{marginVertical:10, marginLeft:20, marginRight: 15, width:50, height:50, borderRadius:45}}
-                    imageStyle={{borderRadius:45}}
-                    source={this.props.imageUri}  >
-                        {image && <Image source={{ uri: image }} style={{width:50, height:50, borderRadius:45}}/>}
-                </ImageBackground>
-                <Text style={{alignSelf:'center', fontSize: 16}}>
-                    {this.props.name}
-                </Text>
+        return (
+            <View style={{ flex: 1, backgroundColor: 'white' }}>
+                <View style={{ flexDirection: 'row' }}>
+                    <ImageBackground
+                        style={{ marginVertical: 10, marginLeft: 20, marginRight: 15, width: 50, height: 50, borderRadius: 45 }}
+                        imageStyle={{ borderRadius: 45 }}
+                        source={this.props.imageUri && { uri: this.props.imageUri }}  >
+                        {image && <Image source={this.props.imageUri && { uri: this.props.imageUri }} style={{ width: 50, height: 50, borderRadius: 45 }} />}
+                    </ImageBackground>
+                    <Text style={{ alignSelf: 'center', fontSize: 16 }}>
+                        {this.props.name}
+                    </Text>
                 </View>
-             </View>
-                
+            </View>
+
         )
     }
 }
@@ -30,8 +30,8 @@ export default class Members extends Component{
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems:'center',
-        justifyContent:'center',
-        backgroundColor:'#FAF7F0',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#FAF7F0',
     }
 })
