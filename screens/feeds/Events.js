1edd0eeede14dc68d210d09c222bfdb93692ada7
keziatamus <65693754+keziatamus@users.noreply.github.com>
2020-11-11 14:23:06 +0800

    Update Events.js

--- screens/feeds/Events.js
+++ screens/feeds/Events.js
@@ -1,43 +1,58 @@
-import React, { Component} from 'react';
-import { View, Text, StyleSheet, Image, FlatList } from 'react-native';
-import { MaterialCommunityIcons } from '@expo/vector-icons'; 
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import global from '../../global';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
 import { Container, Content, Header, Left, Body, Right, Button } from 'native-base'
 
-class Events extends Component{
-    render(){
-        return(
-            <Container style={{width:'90%', height:300, marginLeft:20}}>
-                    <View style={{flex:3}}>
-                      <Image 
-                      source={this.props.imageUri}  
-                      style={{flex:1, width:null, height:null, resizeMode:'cover', borderRadius:10, marginVertical:10}}
-                      />
-                    </View>
-                    <View style={{flexDirection:'row', justifyContent:'space-between'}}>
-                      <Text style={{fontSize:16, fontWeight:'600'}}>{this.props.title}</Text>
-                      <View style={{flexDirection:'row',justifyContent:'flex-end'}}>
-                        <MaterialCommunityIcons name="account" size={15} color={'black'} />
-                        <Text style={{fontSize:13, fontWeight:'300'}}>{this.props.member}</Text>
-                      </View>
-                      
-                    </View>
-                    <View style={{flex:1, paddingTop:5}}>
-                      <Text style={{fontSize:13, fontWeight:'300'}}>{this.props.date}</Text>
-                      <Text style={{fontSize:14, fontWeight:'300'}}>{this.props.location}</Text>
-                    </View>
-                  </Container>
-        )
-    }
+const Events = ({ imageUri, title, member, date, location }) => {
+  //console.log(imageUri);
+  var uri;
+  var imageRef = global.firebase.storage().ref().child('tags/' + imageUri + '.jpg');
+  imageRef
+    .getDownloadURL()
+    .then((url) => {
+      //from url you can fetched the uploaded image easily
+      uri = url;
+      console.log(title + uri);
+    })
+    .catch((e) => console.log('getting downloadURL of image error => ', e));
+
+
+  return (
+    <Container style={{ width: '90%', height: 300, marginLeft: 20 }}>
+      <View style={{ flex: 3 }}>
+        <Image
+          source={uri && {
+            uri: uri
+          }}
+          style={{ flex: 1, width: null, height: null, resizeMode: 'cover', borderRadius: 10, marginVertical: 10 }}
+        />
+      </View>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+        <Text style={{ fontSize: 16, fontWeight: '600' }}>{title}</Text>
+        <View style={{ flexDirection: 'row', justifyContent: 'flex-end' }}>
+          <MaterialCommunityIcons name="account" size={15} color={'black'} />
+          <Text style={{ fontSize: 13, fontWeight: '300' }}>{member}</Text>
+        </View>
+
+      </View>
+      <View style={{ flex: 1, paddingTop: 5 }}>
+        <Text style={{ fontSize: 13, fontWeight: '300' }}>{date}</Text>
+        <Text style={{ fontSize: 14, fontWeight: '300' }}>{location}</Text>
+      </View>
+    </Container>
+  )
 }
 
+
 export default Events;
- 
+
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        alignItems:'center',
-        justifyContent:'center',
-    }
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 })
