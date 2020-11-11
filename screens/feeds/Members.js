ff4866f4f19869f24257ebb9eedef00a96771a99
keziatamus <65693754+keziatamus@users.noreply.github.com>
2020-11-11 22:44:49 +0800

    Update Members.js

--- screens/feeds/Members.js
+++ screens/feeds/Members.js
@@ -1,28 +1,64 @@
-import React, { Component} from 'react';
-import { View, Text, StyleSheet, Image, ImageBackground } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 import { Container, Content } from 'native-base'
+import global from '../../global';
 import MembersTemplate from './MembersTemplate';
 
-export default class Members extends Component{
+export default class Members extends Component {
     state = {
         image: null,
-      };
+        members: [],
+        username: "",
+        proimage: "",
+    };
 
-    render(){
-        let { image } = this.state;
-        return(
-            <Container style={{flex:1, backgroundColor: 'white'}}>
-            <Content>
-                <Text style={{marginTop: 15, marginBottom: 10, marginHorizontal: 20, color:'gray'}}>
-                    Event Member(s)
+    componentWillMount() {
+        var membersinfo = []
+        var username, proimage;
+        global.firebase
+            .database()
+            .ref('event/' + global.event + "/members")
+            .on('value', snapshot => {
+                snapshot.forEach((data) => {
+                    var mem = data.val();
+                    global.firebase
+                        .database()
+                        .ref('user/' + mem)
+                        .on('value', snapshot => {
+                            var v = snapshot.val();
+                            username = v.username;
+                            proimage = v.proimage;
+                        });
+                    membersinfo = membersinfo.concat({
+                        id: mem,
+                        username: username,
+                        proimage: proimage
+                    });
+                });
+
+            })
+        this.state.members = this.state.members.concat(membersinfo);
+
+    };
+
+    render() {
+        return (
+            <Container style={{ flex: 1, backgroundColor: 'white' }}>
+                <Content>
+                    <Text style={{ marginTop: 15, marginBottom: 10, marginHorizontal: 20, color: 'gray' }}>
+                        Event Member(s)
                 </Text>
-                <MembersTemplate
-                imageUri={require('../../assets/me.jpg')}
-                name='Jennie Kim'/>
-                 <MembersTemplate
-                imageUri={require('../../assets/rose.jpg')}
-                name='Rose Park'/>
-            </Content>
+                    <FlatList
+                        data={this.state.members}
+                        renderItem={({ item }) =>
+                            (<MembersTemplate
+                                imageUri={item.proimage}
+                                name={item.username} />
+                            )
+                        }
+
+                    />
+                </Content>
             </Container>
         )
     }
@@ -31,8 +67,8 @@ export default class Members extends Component{
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems:'center',
-        justifyContent:'center',
-        backgroundColor:'#FAF7F0',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#FAF7F0',
     }
 })
