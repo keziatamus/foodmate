d4b38d96173f7b12ce7bef912abe6e5adfc7e3a0
keziatamus <65693754+keziatamus@users.noreply.github.com>
2020-11-11 14:18:29 +0800

    Log Out -> Log In Screen

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button, Alert } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator } from '@react-navigation/stack'; 
 import LoginScreen from './screens/LoginScreen';
 import SignUpScreen from './screens/SignUpScreen';
 import ForgotPassword from './screens/ForgotPassword';
@@ -21,9 +21,8 @@ import PastEvents from './screens/feeds/PastEvents';
 import CurrentEvents from './screens/feeds/CurrentEvents';
 import EditProfile from './screens/feeds/EditProfile';
 import PersonalInfo from './screens/feeds/PersonalInfo';
-//import Chat from './screens/feeds/Chat';
 import global from './global';
-import { Feather } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons'; 
 
 const Stack = createStackNavigator();
 
@@ -45,88 +44,88 @@ export default class App extends React.Component {
     return (
       <NavigationContainer>
         <Stack.Navigator initialRouteName={screen}>
-          <Stack.Screen name="Tabs" component={Tabs}
-            options={{ headerShown: false }} />
-          <Stack.Screen name="Log In" component={LoginScreen}
-            options={{ headerShown: false }} />
-          <Stack.Screen name="Sign Up" component={SignUpScreen}
-            options={{ headerShown: false }} />
-          <Stack.Screen name="Forgot Password" component={ForgotPassword}
-            options={{ headerShown: false }} />
-          <Stack.Screen name="Code Confirmation" component={CodeConfirmation}
-            options={{ headerShown: false }} />
-          <Stack.Screen name="Reset Password" component={ResetPassword}
-            options={{ headerShown: false }} />
-          <Stack.Screen name="Home" component={HomePage}
-            options={{ headerTitle: "Foodmate" }} />
+          <Stack.Screen name="Tabs" component={Tabs} 
+            options={{ headerShown: false }}/>
+          <Stack.Screen name="Log In" component={LoginScreen} 
+            options={{ headerShown: false }}/>
+          <Stack.Screen name="Sign Up" component={SignUpScreen} 
+            options={{ headerShown: false }}/>
+          <Stack.Screen name="Forgot Password" component={ForgotPassword} 
+            options={{ headerShown: false }}/>
+          <Stack.Screen name="Code Confirmation" component={CodeConfirmation} 
+            options={{ headerShown: false }}/>
+          <Stack.Screen name="Reset Password" component={ResetPassword} 
+            options={{ headerShown: false }}/>
+          <Stack.Screen name="Home" component={HomePage} 
+            options={{ headerTitle: "Foodmate" }}/>
           <Stack.Screen name="Foodmate" component={HomePage}
-            options={() => ({
+            options={({route, navigation}) => ({ 
               headerRightContainerStyle: {
                 marginRight: 10
               },
               headerTitle: 'Foodmate',
-              headerRight: () => (
-                <Feather name="log-out" size={20} color="black"
-                  onPress={() =>
-                    Alert.alert(
-                      "Logout",
-                      "Are you sure to log out?",
-                      [
-                        {
-                          text: "Yes",
-                          onPress: () => logout(),
-                        },
-                        {
-                          text: "No",
-                        },
-                      ],
-                      { cancelable: false }
-                    )} />
+              headerRight:()=> (
+                <Feather name="log-out" size={20} color="black" 
+                onPress={() => 
+                  Alert.alert(
+                    "Logout",
+                    "Are you sure to log out?",
+                    [
+                      {
+                        text: "Yes",
+                        onPress: () => navigation.navigate("Log In"),
+                      },
+                      {
+                        text: "No",
+                      },
+                    ],
+                    { cancelable: false }
+                  )}/>
               )
-            })} />
+              })}/>
           <Stack.Screen name="Create" component={CreateEvent} />
-          <Stack.Screen name="View Event" component={ViewEvent}
-            options={{ headerShown: false }} />
-          <Stack.Screen name="Join Event" component={JoinEvent}
-            options={{ headerShown: false }} />
-          <Stack.Screen name="Map" component={MapScreen} />
-          <Stack.Screen name="Set Location" component={InputLocation}
-            options={({ route, navigation }) => ({
+          <Stack.Screen name="View Event" component={ViewEvent} 
+            options={{ headerShown: false }}/>
+          <Stack.Screen name="Join Event" component={JoinEvent} 
+            options={{ headerShown: false }}/>
+          <Stack.Screen name="Map" component={MapScreen}/>
+          <Stack.Screen name="Set Location" component={InputLocation} 
+            options={({route, navigation}) => ({ 
               headerBackTitle: 'Back',
-              headerRight: () => (
-                <Button
-                  onPress={() => navigation.navigate("View Event")}
-                  title="Save‎‎‏‏‎ ‎‏‏‎ ‎‏‏‎" />
+              headerRight:()=> (
+                <Button 
+                onPress={() => navigation.navigate("View Event")}
+                title="Save‎‎‏‏‎ ‎‏‏‎ ‎‏‏‎"/>
               )
-            })} />
-          <Stack.Screen name="Members" component={Members}
-            options={() => ({
-              headerBackTitle: 'Back'
-            })} />
+              })}/>
+          <Stack.Screen name="Members" component={Members} 
+            options={() => ({ 
+              headerBackTitle: 'Back'})}/>
           <Stack.Screen name="Select Category" component={SelectCategory} />
-          <Stack.Screen name="Profile" component={Profile} />
-          <Stack.Screen name="Current Events" component={CurrentEvents}
-            options={() => ({ headerBackTitle: 'Back' })} />
-          <Stack.Screen name="Past Events" component={PastEvents}
-            options={() => ({ headerBackTitle: 'Back' })} />
-          <Stack.Screen name="Edit Profile" component={EditProfile}
-            options={({ route, navigation }) => ({
-              headerBackTitle: 'Back',
-              headerRight: () => (
-                <Button
-                  onPress={() => navigation.navigate("Profile")}
-                  title="Save‎‎‏‏‎ ‎‏‏‎ ‎‏‏‎" />
-              )
-            })} />
-          <Stack.Screen name="Personal Information" component={PersonalInfo}
-            options={({ route, navigation }) => ({
-              headerBackTitle: 'Back',
-              headerRight: () => (
-                <Button
-                  onPress={() => navigation.navigate("View Event")}
-                  title="Save‎‎‏‏‎ ‎‏‏‎ ‎‏‏‎" />
-              )
-            })} />
+          <Stack.Screen name="Profile" component={Profile}/>
+          <Stack.Screen name="Current Events" component={CurrentEvents} 
+            options={() => ({ headerBackTitle: 'Back' })}/>
+          <Stack.Screen name="Past Events" component={PastEvents} 
+            options={() => ({ headerBackTitle: 'Back' })}/>
+          <Stack.Screen name="Edit Profile" component={EditProfile} 
+            options={({route, navigation}) => ({ 
+            headerBackTitle: 'Back',
+            headerRight:()=> (
+              <Button 
+              onPress={() => navigation.navigate("Profile")}
+              title="Save‎‎‏‏‎ ‎‏‏‎ ‎‏‏‎"/>
+            )
+            })}/>
+          <Stack.Screen name="Personal Information" component={PersonalInfo} 
+            options={({route, navigation}) => ({ 
+            headerBackTitle: 'Back',
+            headerRight:()=> (
+              <Button 
+              onPress={() => navigation.navigate("View Event")}
+              title="Save‎‎‏‏‎ ‎‏‏‎ ‎‏‏‎"/>
+            )
+            })}/>
+        
         </Stack.Navigator>
       </NavigationContainer>
     );
