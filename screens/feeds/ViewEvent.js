44bda286695cda0ec9cf51c1f66cdd9875df6e8e
keziatamus <65693754+keziatamus@users.noreply.github.com>
2020-11-11 14:28:46 +0800

    Update ViewEvent.js

--- screens/feeds/ViewEvent.js
+++ screens/feeds/ViewEvent.js
@@ -100,10 +100,17 @@ export default class App extends Component {
           <Text style={styles.description}>{this.state.desc}</Text>
 
           <TouchableOpacity
+            onPress={() => this.props.navigation.goBack()}
+            style={{ position: 'absolute', left: 20, top: 40 }}>
+            <Feather name='arrow-left' size={24} color='#fff' />
+          </TouchableOpacity>
+
+          <TouchableOpacity
             onPress={() => this.props.navigation.navigate("Home")}
             style={{ position: 'absolute', right: 20, top: 40 }}>
             <MaterialIcons name='home' size={24} color='#fff' />
           </TouchableOpacity>
+
         </ImageBackground>
 
         <View style={{}}>
@@ -111,11 +118,6 @@ export default class App extends Component {
             {date(this.state.date)}
             {time(this.state.time)}
             {member(this.state.member)}
-            <TouchableOpacity
-              onPress={() => { this.props.navigation.navigate("Members")}}
-              style={{position:'absolute', left:'90%', top:'65%'}}>
-              <MaterialIcons name="navigate-next" size={30} color='black'/>
-            </TouchableOpacity>
             {location(this.state.place_name)}
           </View>
         </View>
@@ -132,15 +134,15 @@ const styles = StyleSheet.create({
     padding: 20,
   },
   iconContainer: {
-    paddingTop:20,
+    paddingTop: 20,
     flexDirection: 'column',
     flexWrap: 'wrap',
-    alignItems:'flex-start'
+    alignItems: 'flex-start'
   },
   iconStyle: {
     flexDirection: 'row',
     justifyContent: 'flex-start',
-    paddingLeft:20,
+    paddingLeft: 20,
     paddingTop: 20,
   },
   eventTitle: {
@@ -154,29 +156,29 @@ const styles = StyleSheet.create({
     paddingLeft: 10,
   },
   image: {
-    height:300,
-    justifyContent:'flex-end',
+    height: 300,
+    justifyContent: 'flex-end',
   },
   title: {
-    color:'white',
-    fontSize:24,
-    fontWeight:'bold',
-    paddingHorizontal:20,
-    marginVertical:10,
+    color: 'white',
+    fontSize: 24,
+    fontWeight: 'bold',
+    paddingHorizontal: 20,
+    marginVertical: 10,
   },
   description: {
-    color:'white',
-    fontSize:16,
-    fontWeight:'500',
-    paddingHorizontal:20,
-    marginBottom:30
+    color: 'white',
+    fontSize: 16,
+    fontWeight: '500',
+    paddingHorizontal: 20,
+    marginBottom: 30
   },
   imageOverlay: {
-    width:'100%',
-    height:320,
-    borderRadius:20,
-    position:'absolute',
-    backgroundColor:'#000',
-    opacity:0.5,
+    width: '100%',
+    height: 320,
+    borderRadius: 20,
+    position: 'absolute',
+    backgroundColor: '#000',
+    opacity: 0.5,
   },
 });
