491604ba0321d116d1dfae32eaea9f3ff95108cc
keziatamus <65693754+keziatamus@users.noreply.github.com>
2020-11-30 21:33:09 +0800

    Update CurrentEvents.js

--- screens/feeds/CurrentEvents.js
+++ screens/feeds/CurrentEvents.js
@@ -1,41 +1,108 @@
 import React from 'react';
-import { StyleSheet, View, Platform, ScrollView } from 'react-native';
+import { View, FlatList, StyleSheet, Text, TouchableHighlight } from "react-native";
 import Events from './Events';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import global from '../../global'
 
-export default function HomeScreen(props) {
-  
-  return (
-    <View style={styles.container}>
-      <ScrollView>      
-              <TouchableOpacity onPress={()=> props.navigation.navigate('View Event')}>
-                <Events
-                  imageUri={require('../../assets/current_event/1.jpg')}
-                  title="I LOVE PASTA"
-                  member="4"
-                  date="Friday, August 7"
-                  location="里吉拿義大利麵 Regina Pasta"
-                />
-                </TouchableOpacity>
+export default class CurrentEvents extends React.Component {
+  state = {
+    data: this.props.route.params.item,
+    eventdata: null,
+  }
 
-                <TouchableOpacity onPress={()=> props.navigation.navigate('View Event')}>
+  geteventinfo(data) {
+    var item = null;
+    global.firebase
+      .database()
+      .ref('event/' + data)
+      .on('value', snapshot => {
+        item = snapshot.val();
+      })
+    return item;
+  }
+
+  item_pressed(item) {
+    global.event = item;
+    this.props.navigation.navigate("Join Event", { item: item });
+  }
+
+  componentDidMount() {
+    var tmp = [];
+    for (var i in this.state.data) {
+      var itemid = (this.state.data[i]);
+      var info = this.geteventinfo(itemid);
+      tmp.push(info);
+
+    }
+    this.setState({
+      eventdata: tmp,
+    });
+  }
+
+  ListEmptyComponent() {
+    return (
+      <View style={styles.container}>
+
+        <Text style={{
+          fontSize: 17,
+          color: "black",
+          fontWeight: '400',
+          lineHeight: '40',
+          margin: 60,
+          paddingHorizontal: 20,
+          textAlign: 'center'
+        }}>
+          There are no available events.</Text>
+      </View>
+
+    );
+  }
+  render() {
+    console.log(this.state.eventdata);
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={this.state.eventdata}
+          ListEmptyComponent={this.ListEmptyComponent()}
+          renderItem={({ item }) =>
+            (
+              <TouchableOpacity
+                style={styles.events}
+                onPress={() => this.item_pressed(item.eventID)}
+              >
                 <Events
-                  imageUri={require('../../assets/current_event/2.jpg')}
-                  title="Korean Food"
-                  member="4"
-                  date="Friday, August 9"
-                  location="Unni's Korean Kitchen"
-                />
-                </TouchableOpacity>
-          </ScrollView>
-    </View>
-  ) 
+                  imageUri={item.image}
+                  title={item.title}
+                  member={item.member}
+                  date={item.date}
+                  location={item.location} />
+              </TouchableOpacity>
+
+            )}
+        />
+      </View>
+    );
+  }
 }
 
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor:'white',
     flex: 1,
-    backgroundColor: 'white',
   },
-})
+  headertext: {
+    padding: 10,
+    fontSize: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  item: {
+    flexDirection: 'row',
+    flex: 1,
+    padding: 5,
+    height: 42,
+    alignItems: 'center',
+  },
+});
